2c29ea86f8232c9887efa647c8226f39c1c0a2bd
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2021-10-15 19:31:29 +0200

    now libgit done

--- promptprint.c
+++ promptprint.c
@@ -1,6 +1,19 @@
 #define _GNU_SOURCE
 
+// All my precious optimizations are wasted because of libgit2.  The rest of
+// the code runs in less than 0.5ms, and then just git_libgit2_init takes
+// dozens of milliseconds...
+//#define LIBGIT2_SUCKS
+
 #define MAX_LENGTH 10
+
+#include <git2/status.h>
+#include <git2/global.h>
+#include <git2/repository.h>
+#include <git2/buffer.h>
+#include <git2/errors.h>
+#include <git2/graph.h>
+#include <git2/branch.h>
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -14,6 +27,21 @@
 #define WHITE "\033[01;37m"
 #define CADET "\033[00;36m"
 #define RESET_COLOR "\033[00m"
+
+#ifdef LIBGIT2_SUCKS
+static struct timespec t;
+static double start;
+
+static void print_timer(const char *what)
+{
+    clock_gettime(CLOCK_MONOTONIC, &t);
+    double now = t.tv_sec * 1000. + t.tv_nsec * 0.000001;
+    fprintf(stderr, "\n - %s %.2f ms\n", what, now - start);
+    start = now;
+}
+#else
+#define print_timer(x)
+#endif
 
 void print_elided(char *path)
 {
@@ -84,8 +112,106 @@ static void print_path()
     free(cwd);
 }
 
+void print_git_status(git_repository *repo)
+{
+    git_status_options statusopt;
+    git_status_options_init(&statusopt, GIT_STATUS_OPTIONS_VERSION);
+    statusopt.show = GIT_STATUS_SHOW_INDEX_AND_WORKDIR;
+    statusopt.flags = GIT_STATUS_OPT_EXCLUDE_SUBMODULES | GIT_STATUS_OPT_NO_REFRESH;
+
+    git_status_list *status = NULL;
+    int ret = git_status_list_new(&status, repo, &statusopt);
+    if (ret == 0 && git_status_list_entrycount(status)) {
+        printf(" dirty");
+    }
+    git_status_list_free(status);
+}
+
+void print_git_ahead_behind(git_repository *repo, git_reference *head)
+{
+    git_reference *upstream_branch = NULL;
+    int ret = git_branch_upstream(&upstream_branch, head);
+    if (ret != 0 || !upstream_branch) {
+        return;
+    }
+
+    const git_oid *local = git_reference_target(head);
+    const git_oid *upstream = git_reference_target(upstream_branch);
+
+    if (!local || !upstream) {
+        return;
+    }
+
+    size_t ahead = 0, behind = 0;
+    ret = git_graph_ahead_behind(&ahead, &behind, repo, local, upstream);
+    if (ret != 0) {
+        return;
+    }
+    git_reference_free(upstream_branch);
+    if (!ahead && !behind) {
+        return;
+    }
+    printf(" [");
+    if (ahead) {
+        printf("%d ahead", (int)ahead);
+
+        if (behind) {
+            printf(", ");
+        }
+    }
+    if (behind) {
+        printf("%d behind", (int)behind);
+    }
+    printf("]");
+}
+
+void print_git_branch(git_repository *repo)
+{
+    if (git_repository_head_detached(repo)) {
+        printf("detached");
+        return;
+    }
+
+    git_reference *head = NULL;
+    int ret = git_repository_head(&head, repo);
+    if (ret != 0) {
+        return;
+    }
+    const char *name = git_reference_shorthand(head);
+    printf("%s", name);
+    print_git_ahead_behind(repo, head);
+    git_reference_free(head);
+}
+
+void print_git()
+{
+    print_timer("pre git init");
+    git_libgit2_init();
+    print_timer("git init");
+
+    char *cwd = get_current_dir_name();
+    git_repository *repo = NULL;
+    int ret = git_repository_open_ext(&repo, cwd, 0, cwd);
+    free(cwd);
+    if (ret != 0) {
+        return;
+    }
+    printf(CADET " (");
+    print_git_branch(repo);
+    print_timer("branch");
+    print_git_status(repo);
+    print_timer("status");
+    git_repository_free(repo);
+    printf(") ");
+}
+
 int main(int argc, char *argv[])
 {
+#ifdef LIBGIT2_SUCKS
+    clock_gettime(CLOCK_MONOTONIC, &t);
+    start = t.tv_sec * 1000. + t.tv_nsec * 0.000001;
+#endif
+
     (void)argc; (void)argv;
 
     char buf[200];
@@ -103,9 +229,14 @@ int main(int argc, char *argv[])
         buf[(sizeof buf) - 1] = '\0';
         printf("%s", buf);
     }
+
     printf("%s: " WHITE, geteuid() == 0 ? RED : GREEN);
+    print_timer("asic");
 
     print_path();
+    print_timer("path");
+    print_git();
+    print_timer("git total");
 
     printf(RESET_COLOR);
 
